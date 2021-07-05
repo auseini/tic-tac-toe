5a820bde37c9837c8ba9d5b325d01479104f2426
Arian Useini <useini.1@osu.edu>
2021-07-05 16:35:22 -0500

    Have basic layout of webpage complete, need to work on playing the game

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// array for board
+let boardArr = ["x", "x", "o","x", "x", "o","x", "x", "o"];
+
+
+//board module
+const boardObj = (() =>{
+	//create div for board
+	const board = document.createElement("div");
+	board.setAttribute("class", "board");
+	//create divs for each square and set classes
+	
+
+
+
+})();
+
+//function to update board
+function updateBoard(){
+
+}
